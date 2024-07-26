d3392de1650148f550d0c57aaa5ffc844958f9bf
Junanjunan <taltalrealty@gmail.com>
2024-07-26 11:53:21 +0900

    [add] axios interceptors 를 통한 토큰 관리
    1. access_token 만료시 refresh_token으로 갱신
    2. refresh_token 만료시 보안 정보 삭제

--- src/services/api/ServerApi.js
+++ src/services/api/ServerApi.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import Config from 'react-native-config';
+import { getTokens, saveTokens, deleteAllSecureData } from '../../utils/authFunc';
 
 const baseUrl = `${Config.SERVER_URL}/api/v1`;
 
@@ -11,6 +12,54 @@ export const serverApi = axios.create({
   },
 });
 
+serverApi.interceptors.request.use(
+  async (config) => {
+    const tokens = await getTokens();
+    if (tokens && tokens.access_token) {
+      config.headers.Authorization = `Bearer ${tokens.access_token}`;
+    }
+    return config;
+  },
+
+  (error) => Promise.reject(error)
+);
+
+serverApi.interceptors.response.use(
+  (response) => response,
+
+  async (error) => {
+    const originalRequest = error.config;
+    const tokens = await getTokens();
+
+    if (error.response.status === 401 && tokens && tokens.refresh_token) {
+      try {
+        const newTokens = await renewTokenRequest(tokens.refresh_token);
+        const { access_token, refresh_token } = newTokens;
+        const saveTokenResult = await saveTokens(access_token, refresh_token);
+        if (!saveTokenResult.isSuccess) {
+          console.error('Failed to save renewed tokens');
+          return;
+        }
+        originalRequest.headers.Authorization = `Bearer ${access_token}`;
+        return serverApi(originalRequest);
+      } catch(error) {
+        const response = error.response;
+        if (response.status === 401 && response.data.detail.includes("Token has expired")) {
+          const result = await deleteAllSecureData();
+          if (result.isSuccess) {
+            console.log('Deleted all secure data successfully in response interceptor');
+          } else {
+            console.error('Failed to delete all secure data');
+          }
+        } else {
+          console.error(error);
+        }
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
 export const loginRequest = async (username, password) => {
   try {
     const response = await serverApi.post('/token',
@@ -21,6 +70,23 @@ export const loginRequest = async (username, password) => {
   } catch (error) {
     throw error;
   }
+}
+
+export const renewTokenRequest = async (refresh_token) =>{
+  const renewTokenApi = axios.create({
+    baseURL: baseUrl
+  })
+
+  return renewTokenApi.post(
+    "/token/refresh",
+    { refresh_token: refresh_token },
+    { headers: {"Content-Type": "application/x-www-form-urlencoded"} }
+  )
+  .then(res => res.data)
+  .catch(error => {
+    console.error(error);
+    throw error;
+  });
 }
 
 export const fetchBoardNewDataRequest = async (bo_table, params) => {
--- src/utils/authFunc.js
+++ src/utils/authFunc.js
@@ -14,8 +14,10 @@ export const saveTokens = async (access_token, refresh_token) => {
   try {
     await Keychain.setInternetCredentials('auth_tokens', access_token, refresh_token);
     console.log('Tokens saved successfully');
+    return { isSuccess: true };
   } catch (error) {
     console.error('Error saving tokens', error);
+    return { isSuccess: false };
   }
 };
 
@@ -45,5 +47,27 @@ export const getCredentials = async () => {
   } catch (error) {
     console.error('Error retrieving user credentials', error);
     return null;
+  }
+};
+
+export const getTokens = async () => {
+  try {
+    const tokens = await Keychain.getInternetCredentials('auth_tokens');
+    return tokens ? { access_token: tokens.username, refresh_token: tokens.password } : null;
+  } catch (error) {
+    console.error('Error retrieving tokens', error);
+    return null;
+  }
+};
+
+export const deleteAllSecureData = async () => {
+  try {
+    await Keychain.resetInternetCredentials('user_credentials');
+    await Keychain.resetInternetCredentials('auth_tokens');
+    console.log('All secure data deleted successfully');
+    return {isSuccess: true};
+  } catch (error) {
+    console.error('Error deleting secure data', error);
+    return {isSuccess: false};
   }
 };
