8021db3937424270fac3ce6161f903e58a9628e4
Junanjunan <taltalrealty@gmail.com>
2024-08-07 15:00:33 +0900

    [fix] context 기능별 분리 > 사용하지 않는 기존 context 삭제

--- App.js
+++ App.js
@@ -3,21 +3,18 @@ import { SafeAreaView, StyleSheet } from 'react-native';
 import AppNavigator from './src/navigation/AppNavigator';
 import { AuthProvider } from './src/context/auth/AuthContext';
 import { WriteListRefreshProvider, WriteRefreshProvider } from './src/context/refresh/write/RefreshContext';
-import { RefreshProvider } from './src/auth/context/RefreshContext';
 
 const App = () => {
   return (
-    <RefreshProvider>
-      <AuthProvider>
-        <WriteListRefreshProvider>
-        <WriteRefreshProvider>
-          <SafeAreaView style={styles.container}>
-            <AppNavigator />
-          </SafeAreaView>
-        </WriteRefreshProvider>
-        </WriteListRefreshProvider>
-      </AuthProvider>
-    </RefreshProvider>
+    <AuthProvider>
+      <WriteListRefreshProvider>
+      <WriteRefreshProvider>
+        <SafeAreaView style={styles.container}>
+          <AppNavigator />
+        </SafeAreaView>
+      </WriteRefreshProvider>
+      </WriteListRefreshProvider>
+    </AuthProvider>
   );
 };
 
