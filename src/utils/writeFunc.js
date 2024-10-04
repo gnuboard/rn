eae3b64de0ef68a79a9b06f6c2552bcbf944694c
Junanjunan <taltalrealty@gmail.com>
2024-10-04 10:48:59 +0900

    [add] 검색 api - 댓글 검색 이후 원글 이동 로직 추가

--- src/utils/writeFunc.js
+++ src/utils/writeFunc.js
@@ -1,6 +1,22 @@
+import { apiConfig } from "../services/api/config/ServerApiConfig";
+
 export function readWrite(
   bo_table, write, setModalVisible, setModalWrId, navigation
 ) {
+  if (write.type === 'comment') {
+    const order = write.comment_order;
+    const commentPage = Math.ceil(order / apiConfig.commentsPerPage);
+    navigation.navigate(
+      'Boards',
+      {
+        screen: 'Write',
+        params: {bo_table, 'wr_id': write.wr_parent, 'comment_id': write.wr_id, commentPage},
+        initial: false,
+      },
+    )
+    return;
+  }
+
   if (write.wr_option.includes('secret')) {
     setModalWrId(write.wr_id);
     setModalVisible(true);
