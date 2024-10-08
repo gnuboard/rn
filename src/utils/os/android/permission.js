131207fdc9fae54d4af02cd88b2beabb73640007
Junanjunan <taltalrealty@gmail.com>
2024-10-08 15:03:16 +0900

    [fix] 저장공간 액세스 권한 요청 문구 수정 및 버튼 추가

--- src/utils/os/android/permission.js
+++ src/utils/os/android/permission.js
@@ -6,7 +6,9 @@ export const requestStoragePermission = async () => {
       PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
       {
         title: '저장공간 액세스 권한 요청',
-        message: '파일을 다운로드하려면 저장공간에 액세스해야 합니다.',
+        message: '파일을 다운로드하려면 저장공간에 액세스해야 합니다. 허용하시겠습니까?',
+        buttonNegative: "아니오",
+        buttonPositive: "네"
       }
     );
     return granted === PermissionsAndroid.RESULTS.GRANTED;
